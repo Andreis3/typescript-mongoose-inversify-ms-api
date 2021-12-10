fdabf30ed1bd9cc4a3945b35bc7f93786f97c7dd
Andreis3 <andrei.as3@hotmail.com>
2021-12-10 00:08:00 -0300

    chore: config module alias

--- src/application/services/user/UserService.ts
+++ src/application/services/user/UserService.ts
@@ -1,9 +1,9 @@
 import { inject, injectable } from 'inversify';
 import { IUserModel } from 'application/domain/user/IUserModel';
-import TYPES from '../../../infra/constants/Type';
-import { IUserEntity } from '../../../repository/entities/user/IUserEntity';
-import { IUserRepository } from '../../../repository/user-repository/interface/IUserRepository';
-import { IUserService } from './interface/IUserService';
+import TYPES from '@/infra/constants/Type';
+import { IUserEntity } from '@/repository/entities/user/IUserEntity';
+import { IUserRepository } from '@/repository/user-repository/interface/IUserRepository';
+import { IUserService } from '@/application/services/user/interface/IUserService';
 
 @injectable()
 export class UserService implements IUserService {
--- src/application/use-case/RegisterUseCase.ts
+++ src/application/use-case/RegisterUseCase.ts
@@ -1,9 +1,9 @@
 import { inject, injectable } from 'inversify';
-import { IUserModel } from '../../application/domain/user/IUserModel';
-import { UseCase } from '../../infra/base';
-import TYPES from '../../infra/constants/Type';
-import { IUserService } from '../../application/services/user/interface/IUserService';
-import { IUserEntity } from '../../repository/entities/user/IUserEntity';
+import { IUserModel } from '@/application/domain/user/IUserModel';
+import { UseCase } from '@/infra/base';
+import TYPES from '@/infra/constants/Type';
+import { IUserService } from '@/application/services/user/interface/IUserService';
+import { IUserEntity } from '@/repository/entities/user/IUserEntity';
 
 @injectable()
 export class RegisterUseCase implements UseCase<IUserModel, IUserEntity> {
